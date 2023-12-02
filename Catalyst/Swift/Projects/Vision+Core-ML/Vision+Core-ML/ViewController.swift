cc34f3f4a65a755be05bb773feee1645b3f1b35f
Atikul Gazi <atiksplanet@gmail.com>
2023-12-03 03:36:38 +0600

    Vision+Core-ML project added to workspace

---
+++ Catalyst/Swift/Projects/Vision+Core-ML/Vision+Core-ML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vision+Core-ML
+//
+//  Created by Atikul Gazi on 3/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
