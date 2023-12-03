ef067a6888fa7bdf93a1ca011062ec82d066215d
Atikul Gazi <atiksplanet@gmail.com>
2023-12-04 00:28:20 +0600

    SymbioteSyntax Cpp project added

---
+++ Catalyst/C++/SymbioteSyntax/SymbioteSyntax/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SymbioteSyntax
+//
+//  Created by Atikul Gazi on 4/12/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
